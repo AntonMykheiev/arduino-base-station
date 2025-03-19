a3fa4fa93af7cc14fbcdf5a8999283ee975428a1
AntonMykheiev <anton.mykheiev@gmail.com>
2025-03-19 14:07:20 +0100

    ref

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,55 @@
-#include <ArduinoJson.h>
-#include <WiFiS3.h>
+#include <WiFi.h>
 #include <WiFiUdp.h>
+
+#define SERIAL_RTK Serial1
 
 const char* ssid = "";
 const char* password = "";
 
-WiFiUDP udp;
 const char* udpAddress = "";
 const int udpPort = 00000;
 
-#define RTCM_SERIAL Serial1
-#define BUFFER_SIZE 512
+static uint8_t rtcmBuffer[1024];
+static size_t index = 0;
+
+WiFiUDP udp;
 
 void setup() {
-  RTCM_SERIAL.begin(115200);
+  Serial.begin(115200);
+  SERIAL_RTK.begin(115200);
   WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi...");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println(" Connected!");
+
   udp.begin(udpPort);
 }
 
 void loop() {
-  static uint8_t buffer[BUFFER_SIZE];
-  static size_t index = 0;
+  while (SERIAL_RTK.available()) {
+    uint8_t data = SERIAL_RTK.read();
 
-  while (RTCM_SERIAL.available()) {
-    uint8_t byte = RTCM_SERIAL.read();
-    buffer[index++] = byte;
+    if (index == 0 && data != 0xD3) continue;
 
-    if (index >= BUFFER_SIZE || (index > 3 && buffer[0] == 0xD3)) {
-      udp.beginPacket(udpAddress, udpPort);
-      udp.write(buffer, index);
-      udp.endPacket();
+    rtcmBuffer[index++] = data;
 
-      index = 0;
+    if (index > 2) {
+      uint16_t length = ((rtcmBuffer[1] & 0x03) << 8) | rtcmBuffer[2];
+
+      if (index >= length + 6) {
+        udp.beginPacket(udpAddress, udpPort);
+        udp.write(rtcmBuffer, index);
+        udp.endPacket();
+        Serial.println("RTCM data sent!");
+
+        index = 0;
+      }
     }
   }
 }
