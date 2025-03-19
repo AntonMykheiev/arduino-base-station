39abe9929a9e74e78f0e4b806b01af9dd4d4baf8
AntonMykheiev <anton.mykheiev@gmail.com>
2025-03-19 13:06:10 +0100

    RTCM messages instead of NMEA

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,37 @@
 #include <ArduinoJson.h>
-#include <SoftwareSerial.h>
-#include <TinyGPSPlus.h>
-#include <WiFi.h>
+#include <WiFiS3.h>
 #include <WiFiUdp.h>
 
-// Replace with your GPS module RX and TX pins
-#define RXpin A0
-#define TXpin A0
-
-// Replace with your base station coordinates
-#define BASE_LAT 00.000000
-#define BASE_LON 00.000000
-
-// Replace with your SSID and password
 const char* ssid = "";
 const char* password = "";
 
-// Replace with your UDP server address and port
-const char* udpAddress = "";
-const int udpPort = 0;
-
 WiFiUDP udp;
+const char* udpAddress = "";
+const int udpPort = 00000;
 
-SoftwareSerial GPS(RXpin, TXpin);
-TinyGPSPlus gps;
-
-void processGPSData() {
-  if (gps.location.isUpdated()) {
-    double lat = gps.location.lat();
-    double lon = gps.location.lng();
-
-    StaticJsonDocument<200> doc;
-
-    doc["device"] = "base";
-    doc["time"] = gps.time.value();
-    doc["lat_error"] = String((lat - BASE_LAT), 8);
-    doc["lon_error"] = String((lon - BASE_LON), 8);
-
-    String jsonString;
-    serializeJson(doc, jsonString);
-
-    udp.beginPacket(udpAddress, udpPort);
-    udp.print(jsonString);
-    udp.endPacket();
-
-    Serial.print(jsonString);
-  }
-}
+#define RTCM_SERIAL Serial1
+#define BUFFER_SIZE 512
 
 void setup() {
-  GPS.begin(115200);
-  Serial.begin(115200);
+  RTCM_SERIAL.begin(115200);
   WiFi.begin(ssid, password);
   udp.begin(udpPort);
 }
 
 void loop() {
-  while (GPS.available()) {
-    gps.encode(GPS.read());
-  }
+  static uint8_t buffer[BUFFER_SIZE];
+  static size_t index = 0;
 
-  processGPSData();
+  while (RTCM_SERIAL.available()) {
+    uint8_t byte = RTCM_SERIAL.read();
+    buffer[index++] = byte;
+
+    if (index >= BUFFER_SIZE || (index > 3 && buffer[0] == 0xD3)) {
+      udp.beginPacket(udpAddress, udpPort);
+      udp.write(buffer, index);
+      udp.endPacket();
+
+      index = 0;
+    }
+  }
 }
